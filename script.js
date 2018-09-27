8c116ecd4a879efff87feb1c6d4c7c53e7c7ef20
Gabester0 <gceipper@gmail.com>
2018-09-27 14:54:29 -0400

    Most of the full-page CSS

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let navLinks = document.querySelectorAll(".nav-link");
+let navLinks = document.querySelectorAll("#navbar span");
 let sections = document.querySelectorAll(".main-section");
-
+console.log(navLinks);
 let prev;
 let cur = [];
 
@@ -8,53 +8,22 @@ window.addEventListener("scroll", e =>{
 
     let fromTop = window.scrollY;
 
-    navLinks.forEach(link =>{
-        let section = document.querySelector(link.hash);
+    navLinks.forEach(span =>{
+        let section = document.querySelector(span.childNodes[0].childNodes[0].parentElement.hash);
 
         if (section.offsetTop <= fromTop &&
             section.offsetTop + section.offsetHeight >= fromTop){
-                link.classList.add("current");
+                span.classList.add("current");
             } else {
-                link.classList.remove("current");
+                span.classList.remove("current");
+            }
+            let linkTop = document.getElementById("nav-link-top");
+            
+            if(window.scrollY <= 244){
+                linkTop.classList.add("current");
+            } 
+            if(window.scrollY >= 244) {
+                linkTop.classList.remove("current");
             }
     });
 });
-
-// let top = document.getElementById("#top");
-// let topLink = document.getElementById("#nav-link-top");
-// // let navLinks= document.getElementsByClassName("nav-link");
-// // let topLink = navLinks[0];
-// function focusPocus(el){
-//     let elLink= `${el}Link`;
-//     elLink.style.backgroundColor = "#2d2330";
-//         elLink.style.color = "white";
-
-// }
-
-// top.addEventListener("focus", top =>{
-//     focusPocus(top)    
-// });
-
-// let assumptions = document.getElementById("introductions_and_assumptions");
-
-// assumptions.addEventListener("focus", () =>{
-//     navLinks[0].style.backgroundColor = "#2d2330";
-//     navLinks[0].style.color = "white"
-// });
-/*
-"#top"
-use onfocus onblur, add event listener
-="#introduction_and_assumptions"
-="#installation_guide"
-="#navigating_the_terminal"
-="#creating_and_initiating"
-="#adding_and_committing"
-="#branching"
-="#configuring/setup"
-="#creating_a_github_repository"
-="#adding_a_remote_url"
-="#pushing_your_branch" 
-="#pull_request"
-="#pulling_master"
-="#final_steps"
-*/
